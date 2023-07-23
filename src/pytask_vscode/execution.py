532aed3a878c295536e84ba01e8c632ae09a7202
mj023 <max.jahn45@gmail.com>
2023-07-23 23:08:01 +0200

    Added try except

--- src/pytask_vscode/execution.py
+++ src/pytask_vscode/execution.py
@@ -5,18 +5,22 @@ from multiprocessing.connection import Client
 
 @pytask.hookimpl(tryfirst=True)
 def pytask_execute_task_log_end(session: pytask.Session, report: pytask.ExecutionReport) -> None:
-    
-    if not session.config["dry_run"]:
-        attrs = {"type" : "task" ,"name" : report.task.short_name, "path" : str(report.task.path), "report" : str(report.outcome)}
-        address = ('localhost', 6000)
-        conn = Client(address, authkey=b'secret password')
-        conn.send(json.dumps(attrs))
-        conn.close()
-
+    try:
+        if not session.config["dry_run"]:
+            attrs = {"type" : "task" ,"name" : report.task.short_name, "path" : str(report.task.path), "report" : str(report.outcome)}
+            address = ('localhost', 6000)
+            conn = Client(address, authkey=b'secret password')
+            conn.send(json.dumps(attrs))
+            conn.close()
+    except Exception as e:
+        pass
 @pytask.hookimpl
 def pytask_execute_log_end(session: pytask.Session, reports: list[pytask.ExecutionReport]) -> None:
-    if  not session.config["dry_run"]:
-        address = ('localhost', 6000)
-        conn = Client(address, authkey=b'secret password')
-        conn.send('close')
-        conn.close()
+    try:
+        if  not session.config["dry_run"]:
+            address = ('localhost', 6000)
+            conn = Client(address, authkey=b'secret password')
+            conn.send('close')
+            conn.close()
+    except Exception as e:
+        pass
