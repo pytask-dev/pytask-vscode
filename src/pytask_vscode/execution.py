3dfd816250fc321105a0db29a6c70fefe797fee0
mj023 <max.jahn45@gmail.com>
2023-07-26 16:39:48 +0200

    Added Http communication

--- src/pytask_vscode/execution.py
+++ src/pytask_vscode/execution.py
@@ -1,22 +1,23 @@
 import pytask
 import json
+import requests
 from multiprocessing.connection import Client
+
+@pytask.hookimpl(tryfirst=True)
+def pytask_collect_log(session: pytask.Session, reports: list[pytask.CollectionReport], tasks: list[pytask.Task]) -> None:
+        try:    
+            if session.config['command'] == 'collect':
+                result = [{'name' : task.short_name, 'path' : str(task.path)} for task in tasks]
+                res = requests.post('http://localhost:6000/pytask', json={"exitcode" : session.exit_code, "tasks": result}, timeout=0.1)
+        except Exception as e:
+            pytask.console.print_exception()
 
 
 @pytask.hookimpl(tryfirst=True)
 def pytask_execute_task_log_end(session: pytask.Session, report: pytask.ExecutionReport) -> None:
     
-    if not session.config["dry_run"]:
-        attrs = {"type" : "task" ,"name" : report.task.short_name, "path" : str(report.task.path), "report" : str(report.outcome)}
-        address = ('localhost', 6000)
-        conn = Client(address, authkey=b'secret password')
-        conn.send(json.dumps(attrs))
-        conn.close()
-
-@pytask.hookimpl
-def pytask_execute_log_end(session: pytask.Session, reports: list[pytask.ExecutionReport]) -> None:
-    if  not session.config["dry_run"]:
-        address = ('localhost', 6000)
-        conn = Client(address, authkey=b'secret password')
-        conn.send('close')
-        conn.close()
+    try:    
+        result = {'type': 'task', 'name' : report.task.short_name, 'outcome' : str(report.outcome)}
+        res = requests.post('http://localhost:6000/pytask', json=result)
+    except Exception as e:
+        pytask.console.print_exception()
